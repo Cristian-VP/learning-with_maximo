f6f7769347387c4cab4491dae9b6f83a78be3968
Cristian-VP <cvacacelaprocel@cifpfibmoll.eu>
2024-10-06 18:44:08 +0200

    Ejercicio onicial.

---
+++ 00-hello_world/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        /**
+         * Muestra por pantalla la frase "¡Hola mundo!"
+         *
+         * @author Cristian Vacacela
+         */
+        System.out.println("Hello world! My name in Cristian Vacacela, but I prefer use a nickname like Tian.");
+    }
+}
