c403c26c5bc71c418ac9606e69f8a3fd6987a2ea
Cristian-VP <cvacacelaprocel@cifpfibmoll.eu>
2024-10-11 09:47:20 +0200

    Sprint Main.java añadido y README modificado(eliminé un metodo y renombre variables)

--- spring-2-piscina/README.md
+++ spring-2-piscina/README.md
@@ -3,37 +3,40 @@ ___
 
     Inicio algoritmo piscina
    
-    Declarar y asignar  
-        largePols := int 300
-        widthPoolA := int 150
-        widthPoolB := int 80
-        deepPoolA := int 20
-        deepPoolB := int 35
-        resultAreaPools := String "El area total de la piscina A es %d y de la piscina B es %d"
-        resultVolumePools := String "El volumen total de la piscina A es %d y de la piscina B es %d"
-        resultValuesBothPools := String "El ancho de las piscinas juntas es %d y el largo es %d"
-        resultAreaBothPools := String "El valor del area de las dos piscinas juntas es %d"
-        resultVolumeBothPools := String "El valor del volumen de las dos piscinas juntas es %d"
-        resultValuesChangedPools := String "Los nuevo valores son: Piscina A tiene un ancho %d , largo %d  y volumen %d  
-         y la piscina B tiene un ancho %d, largo %d y volumen %d"      
-
     Declarar método calcArea(int width, int large)
-        Devuelve cálculo de área
+        Devuelve width * large
     Declarar método calcVolume(int area, int deep)
-        Devuelve cálculo de volumen
-    Declarar método changeDeep(int assingOf, int assingTo)
-        Devuelve nueva asignación  
+        Devuelve area * deep
 
-    Declarar y asignar  
-        areaPoolA := calcArea 
-        areaPoolB :=  calcArea
-        volumePoolA := calVolume
-        volumePoolB := calcVolume
-        widthBothPools := widthPoolA + widthPoolB
-        areaBothPools := calcArea
-        volumeBothPools := volumePoolA + volumePoolB
-        volumeChangedPoolA := calcVolume
-        volumeChangedPoolB := calcVolume  
+    Declarar y asignar
+    cons
+        largePols : int := 300
+        widthPoolA : int := 150
+        widthPoolB : int := 80
+        deepPoolA : int := 20
+        deepPoolB : int := 35
+    fcons
+
+    Declarar y asignar
+    var
+        resultAreaPools : String := "El area total de la piscina A es  y de la piscina B es "
+        resultVolumePools : String := "El volumen total de la piscina A es %d y de la piscina B es "
+        resultValuesBothPools : String := "El ancho de las piscinas juntas es %d y el largo es "
+        resultAreaBothPools : String := "El valor del area de las dos piscinas juntas es "
+        resultVolumeBothPools : String := "El valor del volumen de las dos piscinas juntas es "
+        resultValuesChangedPools : String := "Los nuevo valores son: Piscina A tiene un ancho , largo   y volumen   
+        y la piscina B tiene un ancho, largo y volumen"
+
+        areaPoolA : int := calcArea 
+        areaPoolB : int := calcArea
+        volumePoolA : int := calVolume
+        volumePoolB : int := calcVolume
+        widthBothPools : int := widthPoolA + widthPoolB
+        areaBothPools : int := calcArea
+        volumeBothPools : int := volumePoolA + volumePoolB
+        volumeChangedPoolA : int := calcVolume
+        volumeChangedPoolB : int := calcVolume  
+    fvar  
 
     Mostrar en terminal  
         resultAreaPools 
--- spring-2-piscina/src/Main.java
+++ spring-2-piscina/src/Main.java
@@ -1,5 +1,58 @@
+
 public class Main {
+    //Declaración de métodos.
+    public static int  calcArea(int width, int large){
+        return width*large;
+    }
+
+    public static int calcVolume(int area, int deep){
+        return area*deep;
+    }
+    //Fin de métodos.
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Declarar y asignar.
+            //Constantes.
+        final int largePols = 300;
+        final int widthPoolA = 150;
+        final int widthPoolB = 80;
+        final int deepPoolB = 35;
+        final int deepPoolA = 20;
+
+            //Asignación valores: area, volumen, dimensiones de las piscinas y sus variantes.
+        int areaPoolA = calcArea(widthPoolA, largePols);
+        int areaPoolB = calcArea(widthPoolB, largePols);
+        int volumePoolA = calcVolume(areaPoolA, deepPoolA);
+        int volumePoolB = calcVolume(areaPoolB, deepPoolB);
+        int widthBothPools = widthPoolA + widthPoolB;
+        int areaBothPools = calcArea(widthBothPools, largePols);
+        int volumeBothPools = volumePoolA + volumePoolB;
+        int volumeChangedPoolA = calcVolume(areaPoolA, deepPoolB);
+        int volumeChangedPoolB = calcVolume(areaPoolB, deepPoolA);
+
+            //Asignación resultados con sus valores correspondientes.
+        String taskAreaPools = "El área total de la piscina A es %d m² y de la piscina B es %d m²\n";
+        String resultTaskAreaPools = String.format(taskAreaPools, areaPoolA, areaPoolB) ;
+        String taskVolumePools = "El volumen total de la piscina A es %d m³ y de la piscina B es %d m³\n";
+        String resultTaskVolumePools = String.format(taskVolumePools, volumePoolA, volumePoolB);
+        String taskValuesBothPools = "El ancho de las piscinas juntas es %d m y el largo es %d m\n";
+        String resultTaskValuesBothPools = String.format(taskValuesBothPools, widthBothPools, largePols);
+        String taskAreaBothPools = "El valor del área de las dos piscinas juntas es %d m²\n";
+        String resultTaskAreaBothPools = String.format(taskAreaBothPools, areaBothPools);
+        String taskVolumeBothPools = "El valor del volumen de las dos piscinas juntas es %d m³\n";
+        String resultTaskVolumeBothPools = String.format(taskVolumeBothPools, volumeBothPools);
+        String taskValuesChangedPools = "Los nuevos valores son: Piscina A tiene un ancho %d m, largo %d m  y volumen %d m³ y la piscina B tiene un ancho %d m, largo %d m y volumen %d m³";
+        String resultTaskValuesChangedPools = String.format(taskValuesChangedPools, widthPoolA, largePols, volumeChangedPoolA, widthPoolB, largePols, volumeChangedPoolB);
+        //Fin declaración y asignación.
+
+        //Mostrar en terminal.
+        System.out.println(resultTaskAreaPools);
+        System.out.println(resultTaskVolumePools);
+        System.out.println(resultTaskValuesBothPools);
+        System.out.println(resultTaskAreaBothPools);
+        System.out.println(resultTaskVolumeBothPools);
+        System.out.println(resultTaskValuesChangedPools);
+        //Fin.
+
     }
 }
