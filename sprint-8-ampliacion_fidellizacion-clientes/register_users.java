3a1691969d3915cf6a5c60f409257cb62a377e9a
Cristian-VP <cvacacelaprocel@cifpfibmoll.eu>
2024-11-29 10:43:48 +0100

    Revisado

--- sprint-8-ampliacion_fidellizacion-clientes/register_users.java
+++ sprint-8-ampliacion_fidellizacion-clientes/register_users.java
@@ -3,9 +3,6 @@ import java.util.List;
 import java.util.Scanner;
 
 
-
-
-
 public class register_users {
     public static boolean truthMachine (Scanner input) {
         try {
@@ -26,7 +23,6 @@ public class register_users {
     public static void main(String[] args) throws Exception {
 
 
-
         Scanner scanner = new Scanner(System.in);
         Integer id, age, kind, total, phone;
         /*El array de valores string de la variable "tipo" tiene que garantizar inmutabilidad en su referencia y sus elementos*/
@@ -39,33 +35,33 @@ public class register_users {
         do {
             do {
                 System.out.println("Bienvenido! Debes ingresar todos los datos solicitados");
-                System.out.print("Ingresa tu ID del usuario: ");
-                id = user_shop.checkID(scanner);
+                System.out.print("Ingresa tu ID del usuario (111 - 999): ");
+                id = sprint.user_shop.checkID(scanner);
                 if (id == null) {
                     errorRegister = true;
                     System.out.println("Error! Entrada de dato inválida. Solo se aceptan numeros naturales de 111 a 999 (incluidos).");
                 } else {
-                    System.out.print("Ingresa tu edad: ");
-                    age = user_shop.checkAge(scanner);
+                    System.out.print("Ingresa tu edad (14 - 120): ");
+                    age = sprint.user_shop.checkAge(scanner);
                     if (age == null) {
                         errorRegister = true;
                         System.out.println("Error! Entrada de dato inválida. Solo se aceptan numeros naturales de 14 a 120 (incluidos).");
                     } else {
                         System.out.println("Ingresa el número correspondiente al tipo de venta: \n Venta libre (0) \n Pensionista (1) \n Carnet Jove (2) \n Socio (3)");
-                        kind = user_shop.checkKind(scanner);
+                        kind = sprint.user_shop.checkKind(scanner);
                         if (kind == null) {
                             errorRegister = true;
                             System.out.println("Error! Entrada de dato inválida. Solo se aceptan números naturales de 0 a 3 (incluidos).");
                         } else {
-                            System.out.print("Ingresa el importe de la compra: ");
-                            total = user_shop.checkTotal(scanner);
+                            System.out.print("Ingresa el importe de la compra ( 0 - 1000): ");
+                            total = sprint.user_shop.checkTotal(scanner);
                             if (total == null) {
                                 errorRegister = true;
                                 System.out.println("Error! Entrada de dato inválida. Solo se aceptan números naturales de 0 a 1000 (incluidos).");
                                 throw new Exception("Programa finalizado");
                             } else {
                                 System.out.print("Ingresa tu teléfono sin el prefijo: ");
-                                phone = user_shop.checkPhone(scanner);
+                                phone = sprint.user_shop.checkPhone(scanner);
                                 if (phone == null) {
                                     errorRegister = true;
                                     System.out.println("Error! Entrada de dato inválida. Solo se aceptan números naturales de 111111111 a 999999999 (incluidos).");
@@ -105,14 +101,16 @@ public class register_users {
                 if (errorRegister) {
                     attemps++;
                     errorRegister = false;
-                }else{
+                } else {
                     attemps = 4;
                 }
             } while (attemps < 3);
             attemps = 0;
             System.out.println("Te gustaría introducir otro usuario?");
             continueRegister = truthMachine(scanner);
-        }while (continueRegister);
-
+        } while (continueRegister);
+        if (!continueRegister) {
+            System.out.println("Gracias por completar el registro");
+        }
     }
 }
